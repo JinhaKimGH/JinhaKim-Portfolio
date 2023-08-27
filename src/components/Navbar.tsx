eb26f6858459a2e7f22585544e0e82b8745d7bb5
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2023-08-27 17:08:04 -0400

    🎨 Added styling to the nav items so that there is a text-decoration on hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,9 @@ import DarkModeToggle from "./DarkModeToggle"
  * @returns {JSX.Element} A React JSX element representing the Navbar Component, the navbar component of the website
 */
 export default function Navbar(props: {darkMode: string,  setDarkMode: React.Dispatch<React.SetStateAction<string>>}): JSX.Element{
+    // User Color css variable is set depending on whether the site is in dark or light mode
+    const style = {"--user-color": `${props.darkMode == 'dark' ? '#fff' : '#333'}`} as React.CSSProperties;
+
     return (
         <nav className={`navbar ${props.darkMode}`}>
             {/* Navbar component contains the title, the different sections of the page, and the dark/light mode toggle */}
@@ -16,31 +19,45 @@ export default function Navbar(props: {darkMode: string,  setDarkMode: React.Dis
                 Jinha Kim
             </div>
             <ul className="nav-items">
-                <li className="nav-list-item">
-                    <span className="material-icons">
-                        person
-                    </span>About
+                <li style={style}>
+                    <a className="nav-list-item">
+                        <span className="material-icons">
+                            person
+                        </span>About
+                    </a>
                 </li>
-                <li className="nav-list-item">
-                    <span className="material-icons">
-                        architecture
-                    </span>Skills
+                <li style={style}>
+                    <a className="nav-list-item">
+                        <span className="material-icons">
+                            architecture
+                        </span>Skills
+                    </a>
                 </li>
-                <li className="nav-list-item">
-                    <span className="material-icons">
-                        token
-                    </span>Projects
+
+                <li style={style}>
+                    <a className="nav-list-item">
+                        <span className="material-icons">
+                            token
+                        </span>Projects
+                    </a>
                 </li>
-                <li className="nav-list-item">
-                    <span className="material-icons">
-                        work
-                    </span>Work Experience
+
+                <li style={style}>
+                    <a className="nav-list-item">
+                        <span className="material-icons">
+                            work
+                        </span>Work Experience
+                    </a>
                 </li>
-                <li className="nav-list-item">
-                    <span className="material-icons">
-                        emoji_events
-                    </span>Awards
+
+                <li style={style}>
+                    <a className="nav-list-item">
+                        <span className="material-icons">
+                            emoji_events
+                        </span>Awards
+                    </a>
                 </li>
+
             </ul>
             <DarkModeToggle darkMode={props.darkMode} setDarkMode={props.setDarkMode}/>
         </nav>
