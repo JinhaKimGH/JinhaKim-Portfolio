d55d06a77e3606e0f7eb03572261245083401235
Jinha <jinhakim0704@gmail.com>
2025-03-17 20:17:44 -0400

    Updated BCF project information

--- src/data/ProjectList.tsx
+++ src/data/ProjectList.tsx
@@ -11,6 +11,15 @@ export const projects = [
     },
 
     {
+        name: "Basketball Court Finder",
+        description: "<p><strong>Basketball Court Finder</strong> is a web app that helps users locate basketball courts near them using interactive maps. It allows users to search for courts based on location, view court details, leave reviews, and edit court information, making it easy to find and explore local basketball courts! 🏀</p>",
+        languages: ["React", "TypeScript", "SpringBoot", "Java", "MySQL"],
+        image: "basketballcourtfinder-screenshot.png",
+        link: "https://github.com/JinhaKimGH/Basketball-Court-Finder",
+        live: "https://jinhakimgh.github.io/Basketball-Court-Finder/"
+    },
+
+    {
         name: "Fakebook",
         description: "<p>A <strong>Facebook clone</strong> that allows users to connect, personalize profiles, share updates, interact with posts, and receive friend suggestions. It also integrates third-party Facebook login for seamless access. This app recreates the social networking experience! 📱</p>",
         languages: ["React", "Node.js", "TypeScript", "JavaScript", "HTML", "CSS", "Express.js", "MongoDB"],
@@ -18,14 +27,4 @@ export const projects = [
         link: "https://github.com/JinhaKimGH/Fakebook-App",
         live: "https://jinhakimgh.github.io/Fakebook-Frontend/"
     },
-
-    
-    {
-        name: "Basketball Court Finder",
-        description: "<p><strong>Basketball Court Finder</strong> is a web app that helps users locate basketball courts near them using interactive maps. It allows users to search for courts based on location, view court details, and rate their experiences, making it easy to find and explore local basketball courts! 🏀</p>",
-        languages: ["React", "JavaScript", "Firebase", "HTML", "CSS"],
-        image: "basketballcourtfinder-screenshot.png",
-        link: "https://github.com/JinhaKimGH/Basketball-Court-Finder",
-        live: "https://jinhakimgh.github.io/Basketball-Court-Finder/"
-    }
 ]
