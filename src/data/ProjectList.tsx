36bc53a572e8ad42a31ff4de260285789b3c1a75
Jinha <jinhakim0704@gmail.com>
2026-04-18 17:59:05 -0400

    Update project list link

--- src/data/ProjectList.tsx
+++ src/data/ProjectList.tsx
@@ -6,7 +6,7 @@ export const projects = [
         description: "<p><strong>StaminaSense</strong> is a wearable system that uses machine learning on biometric data like heart rate, movement, and temperature to give personalized training and recovery recommendations, helping coaches reduce injuries and boost performance! 🤖🏃‍♂️</p>",
         languages: ["Python", "Hugging Face", "TypeScript", "React Native", "Node.js", "JavaScript", "HTML", "CSS"],
         image: "staminasense-screenshot.png",
-        link: "",
+        link: "https://github.com/StamiSense",
         live: "",
         progress: true,
     },
